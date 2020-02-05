4f14e630f3517e269dd8039d937e99478d2cd1b6
Kyle Langford <kyle.langford@brunelloinc.com>
2020-02-05 14:59:51 -0500

    removing error that was causing scripts to fail

--- src/js/main.js
+++ src/js/main.js
@@ -7,24 +7,25 @@
     lazyEls.forEach(function(lazyEl) {
       lazyEl.setAttribute('src', lazyEl.getAttribute('data-src'));
     });
-  } else {
-    // Dynamically include a lazy loading library of your choice
-    // Here including vanilla-lazyload
-    // var script = document.createElement('script');
-
-    // script.async = true;
-
-    // script.src =
-    //   'https://cdn.jsdelivr.net/npm/vanilla-lazyload@12.0.0/dist/lazyload.min.js';
-
-    window.lazyLoadOptions = {
-      elements_selector: '[loading=lazy]',
-      use_native: true,
-      //eventually more options here
-    };
-
-    document.body.appendChild(script);
   }
+  // else {
+  // Dynamically include a lazy loading library of your choice
+  // Here including vanilla-lazyload
+  // var script = document.createElement('script');
+
+  // script.async = true;
+
+  // script.src =
+  //   'https://cdn.jsdelivr.net/npm/vanilla-lazyload@12.0.0/dist/lazyload.min.js';
+
+  // window.lazyLoadOptions = {
+  //   elements_selector: '[loading=lazy]',
+  //   use_native: true,
+  //eventually more options here
+  // };
+
+  // document.body.appendChild(script);
+  // }
 })();
 
 // GA
