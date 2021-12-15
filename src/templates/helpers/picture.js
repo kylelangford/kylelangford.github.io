0a5d7e8906c302eb85954f16756381f3d4c1bfd4
Kyle Langford <kyle.langford@brunelloinc.com>
2021-12-15 14:50:18 -0500

    updating front page - portfolio not done

--- src/templates/helpers/picture.js
+++ src/templates/helpers/picture.js
@@ -12,17 +12,18 @@ var Handlebars = require('handlebars');
  * {{picture  "project/creakyOldJPEG.jpg" "lorem ipsum"}}
  *
  * Outputs
- * <picture class="image">
+ * <picture class="image custom-class">
  *   <source srcset="assets/webp/project/creakyOldJPEG.webp" type="image/webp">
  *   <source srcset="assets/img/project/creakyOldJPEG.jpg" type="image/jpeg">
- *   <img src="assets/img/project/creakyOldJPEG.jpg" alt="lorem ipsum">
+ *   <img src="assets/img/project/creakyOldJPEG.jpg" alt="lorem ipsum" loading="lazy">
  * </picture>
  *
  */
 
-module.exports = function(path, alt, options) {
+module.exports = function(path, alt, classList = '', loading = 'lazy') {
   var picture = '';
-
+  var classList = ' ' + classList;
+  var loading = loading; // Lazy, Eager - attr only goes on img element
   var imgPath = '../assets/img/' + path;
   var webpPath = '../assets/webp/';
   var removeExt = path;
@@ -33,15 +34,17 @@ module.exports = function(path, alt, options) {
   removeExt += '.webp';
   webpPath += removeExt;
 
-  // Lazy Loading
-  // Custom Class
-  // Data-src?
-
-  picture += '<picture class="image">\n';
+  picture += '<picture class="media media--image' + classList + '">\n';
   picture += '<source srcset="' + webpPath + '" type="image/webp">\n';
   picture += '<source srcset="' + imgPath + '" type="image/jpeg">\n';
   picture +=
-    '<img src="' + imgPath + '" alt="' + alt + '" class="paper full-width">\n';
+    '<img src="' +
+    imgPath +
+    '" alt="' +
+    alt +
+    '"  loading="' +
+    loading +
+    '" >\n';
   picture += '</picture>';
 
   return new Handlebars.SafeString(picture);
